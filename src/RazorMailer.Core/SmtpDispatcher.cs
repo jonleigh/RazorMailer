b0fabedff4aed653ca536156db192a29cc75a9bc
Jon Leigh <jon@westridgedesign.com>
2016-07-06 09:15:10 +0100

    Jon (#8)
    * Updating sample application to use latest RazorMailer version
    
    * Documenting SmtpDispatcher
    Adding xml doc file to RazorMailer.Core project

--- src/RazorMailer.Core/SmtpDispatcher.cs
+++ src/RazorMailer.Core/SmtpDispatcher.cs
@@ -3,8 +3,15 @@ using System.Threading.Tasks;
 
 namespace RazorMailer.Core
 {
+    /// <summary>
+    /// A simple dispatcher that sends emails using the built in .NET SmtpClient with default settings (i.e using configuration file settings)
+    /// </summary>
     public class SmtpDispatcher : IEmailDispatcher
     {
+        /// <summary>
+        /// Sends a MailMessage using a default instance of the .NET Smtp Client
+        /// </summary>
+        /// <param name="message">The MailMessage to send</param>
         public void Send(MailMessage message)
         {
             using (var smtp = new SmtpClient())
@@ -13,6 +20,10 @@ namespace RazorMailer.Core
             }
         }
 
+        /// <summary>
+        /// Sends a MailMessage asynchronously using a default instance of the .NET Smtp Client
+        /// </summary>
+        /// <param name="message">The MailMessage to send</param>
         public async Task SendAsync(MailMessage message)
         {
             using (var smtp = new SmtpClient())
